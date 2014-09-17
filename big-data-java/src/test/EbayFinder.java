1cd2d362c00a068ccd658c2a3339868db4a5b113
Nadeem Abdul Hamid <nadeem@acm.org>
2014-09-17 09:50:04 -0400

    ebayfinder example

---
+++ big-data-java/src/test/EbayFinder.java
@@ -0,0 +1,61 @@
+package test;
+import big.data.*;
+
+public class EbayFinder {
+
+	public static void main(String[] args) {
+		DataSource ds = DataSource.connect("http://svcs.ebay.com/services/search/FindingService/v1");
+		ds.set("OPERATION-NAME", "findItemsByKeywords").set("SERVICE-VERSION", "1.0.0");
+		ds.set("SECURITY-APPNAME", "...").set("GLOBAL-ID", "EBAY-US");
+		ds.set("format", "xml").set("keywords", "hp 1606");
+		ds.load();
+		ds.printUsageString(true);
+
+		//String[] items = ds.fetchStringArray("searchResult/item/title");
+		//for (String i : items) System.out.println(i);
+
+		Item[] items = ds.fetchArray("test.Item", 
+				"searchResult/item/title",
+				"searchResult/item/primaryCategory/categoryName",
+				"searchResult/item/sellingStatus/bidCount",
+				"searchResult/item/sellingStatus/currentPrice/value",
+				"searchResult/item/listingInfo/listingType");
+		for (Item i : items) System.out.println(i);
+
+
+	}
+
+}
+
+
+class Item {
+	String name;
+	String category;
+	int bids;
+	double price;
+	String listingType;
+
+
+	public Item(String name, String category, int bids, double price,
+			String listingType) {
+		super();
+		this.name = name;
+		this.category = category;
+		this.bids = bids;
+		this.price = price;
+		this.listingType = listingType;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Item [name=" + name + ", category=" + category + ", bids="
+				+ bids + ", price=" + price + ", listingType=" + listingType
+				+ "]";
+	}
+
+
+
+}
